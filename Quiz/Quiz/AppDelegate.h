a93742f178bee2428351af8988dc0def91aadc3d
Juan Obando <juanchopx2@gmail.com>
2015-09-23 21:27:20 -0500

    Quiz project initial structure.

---
+++ Quiz/Quiz/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Quiz
+//
+//  Created by Juan Obando on 9/23/15.
+//  Copyright © 2015 Big Nerd Ranch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
