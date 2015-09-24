33818a69e84dd24ca3225e0d9cb3795def4e8696
Juan Obando <juanchopx2@gmail.com>
2015-09-23 22:19:42 -0500

    QuizViewController added.

---
+++ Quiz/Quiz/BNRQuizViewController.h
@@ -0,0 +1,13 @@
+//
+//  BNRQuizViewController.h
+//  Quiz
+//
+//  Created by Juan Obando on 9/23/15.
+//  Copyright © 2015 Big Nerd Ranch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BNRQuizViewController : UIViewController
+
+@end
