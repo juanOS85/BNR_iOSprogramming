8bcb5164e1fbb96d7f6f2a770023f83f57fedd0f
Juan Obando <juanchopx2@gmail.com>
2015-12-04 22:10:43 -0500

    Class methods (a/k/a Convenience methods).

--- RandomItems/RandomItems/BNRItem.m
+++ RandomItems/RandomItems/BNRItem.m
@@ -10,6 +10,39 @@
 
 @implementation BNRItem
 
++ (instancetype)randomItem
+{
+  // Create an inmutable array of three adjectives
+  NSArray *randomAdjectiveList = @[@"Fluffy", @"Rusty", @"Shiny"];
+
+  // Create an inmutable array of three nouns
+  NSArray *randomNounList = @[@"Bear", @"Spork", @"Mac"];
+
+  // Get the index of a random adjective/noun from the list
+  // NSInteger is not an object, but a type definition for long
+  NSInteger adjectiveIndex = arc4random() % [randomAdjectiveList count];
+  NSInteger nounIndex = arc4random() % [randomNounList count];
+
+  NSString *randomName = [NSString stringWithFormat:@"%@ %@",
+                          randomAdjectiveList[adjectiveIndex],
+                          randomNounList[nounIndex]];
+
+  int randomValue = arc4random() % 100;
+
+  NSString *randomSerialNumber = [NSString stringWithFormat:@"%c%c%c%c%c",
+                                  '0' + arc4random() % 10,
+                                  'A' + arc4random() % 26,
+                                  '0' + arc4random() % 10,
+                                  'A' + arc4random() % 26,
+                                  '0' + arc4random() % 10];
+
+  BNRItem *newItem = [[self alloc] initWithItemName:randomName
+                                     valueInDollars:randomValue
+                                       serialNumber:randomSerialNumber];
+
+  return newItem;
+}
+
 - (instancetype)initWithItemName:(NSString *)name
                   valueInDollars:(int)value
                     serialNumber:(NSString *)sNumber
--- RandomItems/RandomItems/main.m
+++ RandomItems/RandomItems/main.m
@@ -14,34 +14,14 @@ int main(int argc, const char * argv[]) {
     // Create a mutable array object, store its address in items variable
     NSMutableArray *items = [[NSMutableArray alloc] init];
 
-    // Send the message addObject: to the NSMutableArray pointed to
-    // by the variable items, passing the string each time
-    [items addObject:@"One"];
-    [items addObject:@"Two"];
-    [items addObject:@"Three"];
-
-    // Send another message, insertObject:atIndex:, to that same array object
-    [items insertObject:@"Zero" atIndex:0];
-
-    // For every item in the items array
-    for (NSString *item in items) {
-      // Log the description of the item
-      NSLog(@"%@", item);
+    for (int i = 0; i < 10; i += 1) {
+      BNRItem *item = [BNRItem randomItem];
+      [items addObject:item];
     }
 
-    BNRItem *item = [[BNRItem alloc] initWithItemName:@"Red Sofa"
-                                       valueInDollars:100
-                                         serialNumber:@"A1B2C"];
-
-    // The %@ token is replaced with the result of sending
-    // the description message to the corresponding argument.
-    NSLog(@"%@", item);
-
-    BNRItem *itemWithName = [[BNRItem alloc] initWithItemName:@"Blue Sofa"];
-    NSLog(@"%@", itemWithName);
-
-    BNRItem *itemWithNoName = [[BNRItem alloc] init];
-    NSLog(@"%@", itemWithNoName);
+    for (BNRItem *item in items) {
+      NSLog(@"%@", item);
+    }
 
     // Destroy the mutable array object.
     items = nil;
