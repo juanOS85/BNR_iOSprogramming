587f93b8bd3aa3debd273ee760cc20c6c793f111
Juan Obando <juanchopx2@gmail.com>
2015-12-15 15:04:59 -0500

    Hypnosister and Views hierarchy

---
+++ Hypnosister/Hypnosister/BNRHypnosisView.h
@@ -0,0 +1,13 @@
+//
+//  BNRHypnosisView.h
+//  Hypnosister
+//
+//  Created by Juan Obando on 12/15/15.
+//  Copyright © 2015 Juan Obando. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BNRHypnosisView : UIView
+
+@end
---
+++ Hypnosister/Hypnosister/BNRHypnosisView.m
@@ -0,0 +1,21 @@
+//
+//  BNRHypnosisView.m
+//  Hypnosister
+//
+//  Created by Juan Obando on 12/15/15.
+//  Copyright © 2015 Juan Obando. All rights reserved.
+//
+
+#import "BNRHypnosisView.h"
+
+@implementation BNRHypnosisView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
