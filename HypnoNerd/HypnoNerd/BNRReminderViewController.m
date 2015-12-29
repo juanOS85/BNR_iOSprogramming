fa7e7076de0c15535ec6476a92da8366ada4a17a
Juan Obando <juanchopx2@gmail.com>
2015-12-29 12:02:37 -0500

    HypnoNerd: UITabBarController.

--- HypnoNerd/HypnoNerd/BNRReminderViewController.m
+++ HypnoNerd/HypnoNerd/BNRReminderViewController.m
@@ -16,6 +16,26 @@
 
 @implementation BNRReminderViewController
 
+- (instancetype)initWithNibName:(NSString *)nibNameOrNil
+                         bundle:(NSBundle *)nibBundleOrNil
+{
+  self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+
+  if (self) {
+    // Get the tab bar item
+    UITabBarItem *tbi = self.tabBarItem;
+
+    // Give it a label
+    tbi.title = @"Reminder";
+
+    // Give it an image
+    UIImage *i = [UIImage imageNamed:@"Time.png"];
+    tbi.image = i;
+  }
+
+  return self;
+}
+
 - (IBAction)addReminder:(id)sender
 {
   NSDate *date = self.datePicker.date;
