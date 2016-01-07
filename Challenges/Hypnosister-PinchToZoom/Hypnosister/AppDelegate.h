0728398c73a2fac30874523f1e7874e0cb85efd7
Juan Obando <juanchopx2@gmail.com>
2016-01-06 23:11:12 -0500

    Delegation challenge - On-going.

---
+++ Challenges/Hypnosister-PinchToZoom/Hypnosister/AppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.h
+//  Hypnosister
+//
+//  Created by Juan Obando on 12/15/15.
+//  Copyright © 2015 Juan Obando. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
+
